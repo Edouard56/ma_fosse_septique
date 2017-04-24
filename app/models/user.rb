0c5d4283aa502eb8af58dcc62dda39703e63adb9
Edouard Castaignet <ecastaignet@hotmail.com>
2017-04-24 19:21:05 +0200

    trying installing devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :company, presence: :true
+  validates :address, presence: :true
+  validates :mobile_phone, presence: :true
+
 end

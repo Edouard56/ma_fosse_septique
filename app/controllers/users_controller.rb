ecd3d34aade4b4f393b99116c1a6997ad818510e
Edouard Castaignet <ecastaignet@hotmail.com>
2017-04-26 16:06:18 +0200

    new show

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,55 @@
+class UsersController < ApplicationController
+  skip_before_action :authenticate_user!, only: [ :index, :show ]
+  before_action :set_user, only: [:show, :edit, :update]
+
+
+  def index
+    @users = User.all
+
+    @users = User.where.not(latitude: nil, longitude: nil)
+
+    @hash = Gmaps4rails.build_markers(@users) do |user, marker|
+      marker.lat user.latitude
+      marker.lng user.longitude
+    end
+  end
+
+  def show
+
+  end
+
+  # def new
+  #   @user = User.new
+  # end
+
+  # def create
+  #   @user = User.new(user_params)
+  #   @user.save
+
+  #   redirect_to users_path
+  # end
+
+
+  def edit
+     @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+
+    redirect_to user_path(@user)
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:company, :first_name, :last_name, :position, :mobile_phone, :office_phone, :email, :address, :description, :radius, :nettoyage_toiture)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+
   devise_for :users
+
   root to: 'pages#home'
 
   # creation de l'instance client (create) + message (show)
   resources :clients, only: [ :new, :create, :show ]
+
+  resources :users, only: [ :index, :show ]
+
 
   # création de la route formulaire (a faire)
 

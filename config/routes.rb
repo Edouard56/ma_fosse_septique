abd9a62002cb9825afc679ee7f40543319546c10
Edouard Castaignet <ecastaignet@hotmail.com>
2017-04-27 12:00:41 +0200

    nothing

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   # creation de l'instance client (create) + message (show)
   resources :clients, only: [ :new, :create, :show ]
 
+  # En tant que pro visiteur (user),
+  # je peux crud un user
   resources :users, only: [ :index, :show ]
 
   mount Attachinary::Engine => "/attachinary"
